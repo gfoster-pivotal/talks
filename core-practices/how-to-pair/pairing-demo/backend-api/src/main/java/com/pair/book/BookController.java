60c77c8bad569cf70b7e00df6285c61777b099b4
George Foster <georgepfoster@gmail.com>
2016-10-03 14:36:53 -0500

    remove unnecessary files

--- core-practices/how-to-pair/pairing-demo/backend-api/src/main/java/com/pair/book/BookController.java
+++ core-practices/how-to-pair/pairing-demo/backend-api/src/main/java/com/pair/book/BookController.java
@@ -1,9 +1,15 @@
 package com.pair.book;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 import java.util.List;
 
+@RestController
 public class BookController {
+    @RequestMapping(path = "/books")
     public List<Book> getBooks() {
-        return null;
+        return Collections.singletonList(new Book("foo", "bar", "baz"));
     }
 }
--- core-practices/how-to-pair/pairing-demo/backend-api/src/test/java/com/pair/book/BookControllerTest.java
+++ core-practices/how-to-pair/pairing-demo/backend-api/src/test/java/com/pair/book/BookControllerTest.java
@@ -6,10 +6,14 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.Assert.*;
 
 public class BookControllerTest {
     @Rule
@@ -18,8 +22,31 @@ public class BookControllerTest {
     @InjectMocks
     BookController bookController;
 
-    @Test
-    public void given_when_then() {
-        List<Book> books = bookController.getBooks();
+    MockMvc mockMvc;
+
+    @Before
+    public void setUpMockMVC() throws Exception {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(bookController)
+                .build();
     }
+
+    @Test
+    public void is_this_a_good_test() {
+        Book book = bookController.getBooks().get(0);
+        assertThat(book.getTitle()).isEqualToIgnoringCase("foo");
+        assertThat(book.getIsbn()).isEqualToIgnoringCase("bar");
+    }
+
+    @Test
+    public void aSingleBook_getAListOfBooks_validContract() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/books");
+        String actual = mockMvc.perform(requestBuilder)
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        String expected = "[{\"title\":\"foo\",\"isbn\":\"bar\", \"author\":\"baz\"}]";
+        JSONAssert.assertEquals(expected, actual, true);
+    }
+
 }
