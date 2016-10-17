6707755f08879bc2d6f013985c07a6724a75330d
gfoster-pivotal <gfoster@pivotal.io>
2016-10-17 16:55:53 -0500

    add debugging statements

--- core-practices/how-to-pair/pairing-demo/concourse/build-book-producer.sh
+++ core-practices/how-to-pair/pairing-demo/concourse/build-book-producer.sh
@@ -4,5 +4,6 @@ cd pairing-demo/core-practices/how-to-pair/pairing-demo/;
 ls -altr;
 pwd;
 which gradle;
-gradle book-producer:assemble;
-# ./gradlew book-producer:assemble;
+ls -altr gradlew;
+# gradle book-producer:assemble;
+./gradlew book-producer:assemble;
