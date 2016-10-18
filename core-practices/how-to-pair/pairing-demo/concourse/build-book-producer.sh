5d138474cf95261a2024a99854ed219924444082
gfoster-pivotal <gfoster@pivotal.io>
2016-10-18 17:38:01 -0500

    separate into two plans

--- core-practices/how-to-pair/pairing-demo/concourse/build-book-producer.sh
+++ core-practices/how-to-pair/pairing-demo/concourse/build-book-producer.sh
@@ -2,9 +2,4 @@
 
 set -x;
 cd pairing-demo/core-practices/how-to-pair/pairing-demo/;
-ls -altr;
-pwd;
-which gradle;
-ls -altr gradlew;
-./gradlew book-producer:assemble;
 gradle book-producer:assemble;
