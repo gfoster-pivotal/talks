a2059861560b7a06adf68714adc9fa1649315e41
gfoster-pivotal <gfoster@pivotal.io>
2016-10-25 09:35:31 -0500

    push built artifact to gist

--- core-practices/how-to-pair/pairing-demo/concourse/build-book-producer.sh
+++ core-practices/how-to-pair/pairing-demo/concourse/build-book-producer.sh
@@ -3,3 +3,9 @@
 set -x;
 cd book-producer/core-practices/how-to-pair/pairing-demo/;
 gradle book-producer:assemble;
+
+cd -;
+git clone resource-gist updated-gist;
+cp book-producer/core-practices/how-to-pair/pairing-demo/book-producer/build/libs/book-producer-0.0.1-SNAPSHOT.jar updated-gist;
+git add .;
+git commit -m "updating build";
