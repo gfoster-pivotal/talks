1eb30835b8f35dc87624b60e43da1827f032616b
gfoster-pivotal <gfoster@pivotal.io>
2016-10-25 09:39:42 -0500

    setup pcf dev locally

--- core-practices/how-to-pair/pairing-demo/concourse/setup.sh
+++ core-practices/how-to-pair/pairing-demo/concourse/setup.sh
@@ -3,17 +3,19 @@
 fly sp -t demo-api -p demo-api -c pipeline.yml -n;
 fly up -t demo-api -p demo-api;
 
+cf dev destroy;
+cf dev start;
+
 cd ..;
 gradle assemble;
 cf login -a https://api.local.pcfdev.io --skip-ssl-validation -u user -p pass;
 
 cf d book-producer-v1 -f -r;
 cf d book-producer-v2 -f -r;
-cf d consumer-v1 -f -r;
-
 cf p -f book-producer/book-producer-v1_manifest.yml;
 cf p -f book-producer/book-producer-v2_manifest.yml;
 cf map-route book-producer-v1 local.pcfdev.io --hostname book;
 
+cf d consumer-v1 -f -r;
 cf p -f consumer/consumer-v1_manifest.yml;
 cf map-route consumer-v1 local.pcfdev.io --hostname consumer;
